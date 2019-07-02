23cd7ab9011b72ce5688483ac9d7eb5f45aed15a
Ryan <ryandbuskirk@gmail.com>
2019-07-01 20:37:24 -0400

    change

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
 		<Router>
 			<div className="App">
 				<header className="App-header">
-					<Link to="/">Home</Link>
+					<Link to="/">Homes</Link>
 					<Link to="/otherpage">Other Page</Link>
 				</header>
 				<div>
